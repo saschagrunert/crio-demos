d741af46a9e4dc1816d4601191e9342a70b6abf0
Sascha Grunert <sgrunert@suse.com>
2019-11-13 15:46:25 +0100

    Update recovering demo
    Signed-off-by: Sascha Grunert <sgrunert@suse.com>

--- pkg/runs/recovering.go
+++ pkg/runs/recovering.go
@@ -6,7 +6,39 @@ import (
 )
 
 func Recovering(context *cli.Context) {
+	Run(
+		S("kubectl delete events --all"),
+		S(`sudo sed -i -E 's/(log_level = )(.*)/\1"debug"/' /etc/crio/crio.conf`),
+		S("sudo kill -HUP $(pgrep crio)"),
+	)
+
 	d := New("Recovering Workloads")
 
+	d.Step(S(
+		"Let’s start with a fresh nginx deployment",
+	), S(
+		"kubectl create deployment --image=nginx nginx",
+	))
+
+	d.Step(S(
+		"Now we kill the container internal nginx process",
+	), S(
+		"sudo pkill -KILL nginx",
+	))
+
+	d.Step(S(
+		"Then the container monitor conmon will notice that and the workload gets removed",
+	), S(
+		"sudo journalctl -u crio --since '1 minute ago' | grep -A1 'exited with status'",
+	))
+
+	d.Step(S(
+		"The kubelets synchronization loop will notice that as well and will re-schedule the workload",
+	), S(
+		"sudo journalctl -u kubelet --since '1 minute ago' | grep -A1 ContainerDied",
+	))
+
 	d.Run()
+
+	Run(S("kubectl delete deploy nginx"))
 }
